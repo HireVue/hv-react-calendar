3fe0febeba2b68f2907a88196a3c8fd0c8f94118
E Brent Nelson <bnelson@hirevue.com>
2015-01-27 18:57:34 -0800

    Create calendar widget and examples.

--- examples/app.js
+++ examples/app.js
@@ -2,11 +2,42 @@ var React           = require('react');
 var HvReactCalendar = require('hv-react-calendar');
 
 var App = React.createClass({
+
   render: function() {
+
+    var now = new Date();
+    var yesterday = new Date(now.getFullYear(), now.getMonth(), now.getDate()-1);
+    var tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1);
+    var lastMonth = new Date(now.getFullYear(), now.getMonth()-1, now.getDate());
+
+    var dateClasses = [
+      {
+        date: now,
+        classNames: "today"
+      },
+      {
+        date: yesterday,
+        classNames: "yesterday"
+      },
+      {
+        date: tomorrow,
+        classNames: "tomorrow"
+      }
+    ];
+
     return (
-      <HvReactCalendar />
+      <div>
+        <HvReactCalendar />
+        <HvReactCalendar startOfWeek="monday" />
+        <HvReactCalendar currentDate={lastMonth} />
+        <HvReactCalendar locale="fr" />
+        <HvReactCalendar forceSixRows={true} />
+        <HvReactCalendar dateClasses={dateClasses} />
+      </div>
     );
+
   }
+
 });
 
 React.render(<App />, document.getElementsByTagName('body')[0]);
--- modules/HvReactCalendar.js
+++ modules/HvReactCalendar.js
@@ -1,10 +1,133 @@
-var React = require('react');
+var moment    = require('moment');
+var React     = require('react');
+var PropTypes = React.PropTypes;
+
 
 var HvReactCalendar = React.createClass({
+
+  propTypes: {
+    locale       : PropTypes.string,
+    startOfWeek  : PropTypes.oneOf(['sunday','monday']).isRequired,
+    currentDate  : PropTypes.instanceOf(Date),
+    forceSixRows : PropTypes.bool,
+    dateClasses  : PropTypes.arrayOf(PropTypes.shape({
+      date       : PropTypes.instanceOf(Date),
+      classNames : PropTypes.string
+    }))
+  },
+
+  getDefaultProps: function() {
+    return {
+      locale       : 'en',
+      startOfWeek  : 'sunday',
+      forceSixRows : false
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      date: moment()
+    }
+  },
+
+  componentWillMount: function() {
+    if (this.props.currentDate) {
+      this.setState({date: moment(this.props.currentDate)});
+    }
+  },
+
+  nextMonth: function() {
+    this.setState({date: this.state.date.add(1, 'months')});
+  },
+
+  prevMonth: function() {
+    this.setState({date: this.state.date.subtract(1, 'months')});
+  },
+
+  getMonthName: function() {
+    moment.locale(this.props.locale);
+    return moment.months()[this.state.date.get('month')] + ' ' + this.state.date.get('year');
+  },
+
+  /* make the days of the week */
+  getDaysOfTheWeek: function() {
+    moment.locale(this.props.locale);
+    var weekdays = moment.weekdaysShort();
+    if (this.props.startOfWeek === 'monday') {
+      weekdays.push(weekdays.shift());
+    }
+    return weekdays;
+  },
+
+  /* make the days of the month */
+  getDays: function() {
+    var days = [];
+    var date = this.state.date.startOf('month');
+    var diff = date.weekday();
+    if (this.props.startOfWeek === 'monday') {
+      diff -= 1;
+    }
+    if (diff < 0) diff += 7;
+
+    var i;
+    for (var i = 0; i < diff; i++) {
+      var day = moment([this.state.date.year(), this.state.date.month(), i-diff+1])
+      days.push({day: day, classes: 'prev-month'});
+    }
+
+    var numberOfDays = date.daysInMonth();
+    for (i = 1; i <= numberOfDays; i++) {
+      var day = moment([this.state.date.year(), this.state.date.month(), i]);
+      days.push({day: day});
+    }
+
+    i = 1;
+    while (days.length % 7 !== 0) {
+      var day = moment([this.state.date.year(), this.state.date.month(), numberOfDays+i]);
+      days.push({day: day, classes: 'next-month'});
+      i++;
+    }
+
+    if (this.props.forceSixRows && days.length !== 42) {
+      var start = moment(days[days.length-1].day).add(1, 'days');
+      while (days.length < 42) {
+        days.push({day: moment(start), classes: 'next-month'});
+        start.add(1, 'days');
+      }
+    }
+
+    return days;
+  },
+
   render: function() {
+    var renderDayOfWeek = function(day, i) {
+      return <div key={'day-of-week-' + i} className="calendar__cell --day">{day}</div>;
+    };
+
+    var renderDay = function(day, i) {
+      var classNames = "calendar__cell " + day.classes;
+      return (
+        <div key={'day-' + i} className={classNames}>
+          <span className="calendar__day-label">{day.day.date()}</span>
+        </div>
+      );
+    };
+
     return (
-      <h3>Hello. World.</h3>
-    )
+      <div className="calendar">
+        <div className="calendar__head --controls">
+          <div className="calendar__prev-btn" onClick={this.prevMonth}></div>
+          <div className="calendar__date-display">{this.getMonthName()}</div>
+          <div className="calendar__next-btn" onClick={this.nextMonth}></div>
+        </div>
+        <div className="calendar__head --days-of-week">
+          {this.getDaysOfTheWeek().map(renderDayOfWeek)}
+        </div>
+        <div className='calendar__body'>
+          {this.getDays().map(renderDay)}
+        </div>
+      </div>
+    );
   }
 });
 
